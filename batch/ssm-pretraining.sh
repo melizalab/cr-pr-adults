8606526887b06811eb1bad2bad01282c33733fcc
Dan Meliza <dan@meliza.org>
2024-04-04 14:00:19 -0400

    fix names of ssm batch scripts

--- batch/ssm-pretraining.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-OUTDIR="build"
-INPUTS="inputs/behavior_data.tbl"
-
-mkdir -p ${OUTDIR}
-awk 'NR>1 {gsub(/[0-9]/, "", $2); print $1"_"$2}' inputs/behavior_data.tbl | sort | uniq | \
-    parallel -j4 Rscript scripts/ssm-training.R {}
-
--- batch/ssm-training.sh
+++ batch/ssm-training.sh
@@ -5,5 +5,5 @@ INPUTS="inputs/behavior_data.tbl"
 
 mkdir -p ${OUTDIR}
 awk 'NR>1 {gsub(/[0-9]/, "", $2); print $1"_"$2}' inputs/behavior_data.tbl | sort | uniq | \
-    parallel Rscript scripts/ssm-training.R {}
+    parallel -j4 Rscript scripts/ssm-training.R {}
 
