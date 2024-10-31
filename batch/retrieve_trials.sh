337c7b85d13b75f6022e2cdb4aebc9ebcec3bd55
Dan Meliza <dan@meliza.org>
2024-10-31 13:35:00 -0400

    add reaction time to behavior data

--- batch/retrieve_trials.sh
+++ batch/retrieve_trials.sh
@@ -9,4 +9,4 @@ if [[ -t 0 ]]; then
     exit 1
 fi
 mkdir -p ${OUTDIR}
-parallel --colsep ' +' --skip-first-line -j4 ${PYTHON} -m decide_analysis.get_data -r ${DECIDE_HOST} --fields id,subject,time,trial,stimulus,lights,correct,response,result,correction,experiment --name gng -k experiment__startswith={3} --output ${OUTDIR}/{1}_{2}_trials.csv {1}
+parallel --colsep ' +' --skip-first-line -j4 ${PYTHON} -m decide_analysis.get_data -r ${DECIDE_HOST} --fields id,subject,time,trial,stimulus,lights,correct,response,rtime,result,correction,experiment --name gng -k experiment__startswith={3} --output ${OUTDIR}/{1}_{2}_trials.csv {1}
