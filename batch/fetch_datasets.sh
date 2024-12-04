a772763d76d37d3fc299aba7994adf02e1fb5bb7
Dan Meliza <dan@meliza.org>
2024-12-04 15:12:40 -0500

    update fetch_datasets script

--- batch/fetch_datasets.sh
+++ batch/fetch_datasets.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 set -e
-OUTDIR="datasets/"
+OUTDIR="datasets"
 
 # retrieve the datasets
 
 # check file integrity
-shasum -a 512 -c ${OUTDIR}sha512sums
+shasum -a 512 -c ${OUTDIR}/sha512sums
 
 # unpack the datasets
+for zipfile in ${OUTDIR}/*.zip; do
+    if [ -f "$zipfile" ] ; then
+	unzip -o ${zipfile} -d ${OUTDIR}
+    fi
+done
 
 # delete the zip files
